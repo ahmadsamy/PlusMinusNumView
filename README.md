cf166bdea1d463d454471dea4b06213b70e60ec4
AhmadSamy <a.samy93@gmail.com>
2021-09-15 13:38:02 +0200

    labels refactoring

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # PlusMinusNumView
+
+*Simple widget that has a number with plus button to increase its value 
+and a minus button to decrease it
+
+*Xml styleable options:
+- maxVal(float): maximum value
+- minVal(float): minimum value
+- step(float): step value
+- title(string): title of widget
+- valueTextAppearance(reference): text appearance of value TextView
+
+#XML layout usage
+
+```XML
+<ahmad.egypt.plusminusnumview.PlusMinusNumView
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintLeft_toLeftOf="parent"
+        app:layout_constraintRight_toRightOf="parent"
+        app:layout_constraintTop_toTopOf ="parent"
+        app:numTextAppearance="?android:attr/textAppearanceLarge"
+        app:maxNum="6"
+        app:minNum="1"
+        app:step="1"
+        android:id="@+id/plus_minus_num_view"
+        app:title="Distance"
+        />
+```
+
+#Activity usage
+
+```Kotlin
+//Usage of OnValueChangeListener
+binding.plusMinusNumView.valueChangeListener=object : PlusMinusNumView.OnValueChangeListener{
+            override fun onValueChange(newVal: Float) {
+                    Toast.makeText(applicationContext,"New Value is $newVal",Toast.LENGTH_SHORT).show()
+            }
+        }
+//get current value
+        val currentValue=binding.plusMinusNumView.currentValue
+```
--- src/main/java/ahmad/egypt/plusminusnumview/PlusMinusNumView.kt
+++ src/main/java/ahmad/egypt/plusminusnumview/PlusMinusNumView.kt
@@ -12,30 +12,30 @@ class PlusMinusNumView(context: Context, attrSet: AttributeSet?, defStyleAttr:In
         constructor(context: Context,attrSet: AttributeSet) : this(context,attrSet,0)
         constructor(context: Context): this(context,null,0)
 
-        private var maxNum=6f
-        private var minNum=1f
+        private var maxVal=6f
+        private var minVal=1f
         private var step=1f
         private var title:String?=null
 
         private var plusButton:AppCompatImageView
         private var minusButton:AppCompatImageView
-        private var numTV:TextView
+        private var valueTV:TextView
         private var titleTV:TextView
 
 
-        var currentNum:Float = maxNum
+        var currentValue:Float = maxVal
         get() {
-            return numTV.text.toString().toFloat()
+            return valueTV.text.toString().toFloat()
         }
 
-        var numChangeListener:OnNumChangeListener?=null
+        var valueChangeListener:OnValueChangeListener?=null
 
         init {
         inflate(context, R.layout.plus_minus_num_layout,this)
         plusButton=findViewById(R.id.plus)
         minusButton=findViewById(R.id.minus)
-        numTV=findViewById(R.id.numView)
-        numTV.text=getFormattedNum(maxNum)
+        valueTV=findViewById(R.id.valueView)
+        valueTV.text=formatNumber(maxVal)
         titleTV=findViewById(R.id.title)
 
         context.theme.obtainStyledAttributes(
@@ -43,12 +43,12 @@ class PlusMinusNumView(context: Context, attrSet: AttributeSet?, defStyleAttr:In
             R.styleable.PlusMinusNumView,
             0, 0).apply {
             try {
-                maxNum = getFloat(R.styleable.PlusMinusNumView_maxNum, 6f)
-                minNum = getFloat(R.styleable.PlusMinusNumView_minNum, 1f)
+                maxVal = getFloat(R.styleable.PlusMinusNumView_maxVal, 6f)
+                minVal = getFloat(R.styleable.PlusMinusNumView_minVal, 1f)
                 step = getFloat(R.styleable.PlusMinusNumView_step,1f)
-                val textAppearance = getResourceId(R.styleable.PlusMinusNumView_numTextAppearance,
+                val textAppearance = getResourceId(R.styleable.PlusMinusNumView_valueTextAppearance,
                     android.R.attr.textAppearanceLarge)
-                numTV.setTextAppearance(context,textAppearance)
+                valueTV.setTextAppearance(context,textAppearance)
                 title=getString(R.styleable.PlusMinusNumView_title)
                 if (title!=null){
                     titleTV.text=title
@@ -60,23 +60,23 @@ class PlusMinusNumView(context: Context, attrSet: AttributeSet?, defStyleAttr:In
         }
 
         plusButton.setOnClickListener {
-            setNum(currentNum+step)
+            setVal(currentValue+step)
         }
 
         minusButton.setOnClickListener {
-            setNum(currentNum-step)
+            setVal(currentValue-step)
         }
 
 
     }
 
-    private fun setNum(newNum:Float){
-        if (newNum>maxNum||newNum<minNum)return
-        numTV.text=getFormattedNum(newNum)
-        numChangeListener?.onNumChange(newNum)
+    private fun setVal(newVal:Float){
+        if (newVal>maxVal||newVal<minVal)return
+        valueTV.text=formatNumber(newVal)
+        valueChangeListener?.onValueChange(newVal)
     }
 
-    private fun getFormattedNum(num:Float):String{
+    private fun formatNumber(num:Float):String{
         return if (isInteger(num)) "${num.toInt()}" else "%.2f".format(num)
     }
 
@@ -84,7 +84,7 @@ class PlusMinusNumView(context: Context, attrSet: AttributeSet?, defStyleAttr:In
         return ((num.toInt()*10)-(num*10).toInt()==0)
     }
 
-    interface OnNumChangeListener{
-        fun onNumChange(newNum:Float)
+    interface OnValueChangeListener{
+        fun onValueChange(newVal:Float)
     }
 }
